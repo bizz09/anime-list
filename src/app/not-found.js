3d5f8f6a5941e21c39b4b79c383989b87f5c2f8d
M Izzu Rohman Firdaus <wanjuvada@gmail.com>
2023-10-13 20:06:59 +0800

    update

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,14 +5,14 @@ import React from 'react'
 
 export default function NotFound() {
     return (
-        <div className='min-h-screen max-w-xl mx-auto flex justify-center items-center'>
-            <div className='flex justify-center items-center gap-4 flex-col'>
+        <div className='min-h-screen max-w-xl mx-auto flex flex-col justify-center items-center'>
+            <div className='flex justify-center items-center gap-2 pb-4'>
                 <FileSearch size={32} className='text-accent'/>
-                <h1 className='text-accent font-bold text-2xl'>NOT FOUND</h1>
+                <h1 className='text-accent font-bold text-2xl'>| NOT FOUND</h1>
+            </div>
                 <Link href='/' className='p-2 rounded-md bg-primary text-dark hover:bg-accent hover:text-primary'>
                 Kembali
                 </Link>
-            </div>
         </div>
     )
 }
--- src/app/populer/page.js
+++ src/app/populer/page.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Populer() {
   return (
-    <div>
-      belum ada data
+    <div className='min-h-screen items-center justify-center flex'>
+      <div className='flex justify-center items-center'>
+        <h1 className='text-md md:text-lg text-accent font-bold text-center'>HALAM MASIH DALAM <br/> PENGEMBANGAN 😉💨</h1>
+      </div>
     </div>
   )
 }
