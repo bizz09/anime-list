4963e02e248721c0eaa5da74422c4a15face3c7c
M Izzu Rohman Firdaus <wanjuvada@gmail.com>
2023-10-13 19:48:42 +0800

    update loader

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Loading() {
   return (
     <div className='flex justify-center items-center min-h-screen'>
-        <div className='flex justify-center items-center loading'></div>
+        <div className='flex justify-center items-center '>
+            <div className='loading'>
+
+            </div>
+        </div>
     </div>
   )
 }
